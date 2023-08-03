a0603ce5c11d4dceac474946591c5bcf24d64360
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-03 14:31:59 +0300

    Ders 8 Entitylerin oluşturulması

---
+++ AuthServer.Core/Models/UserApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    public class UserApp : IdentityUser
+    { // hem IdentityUser'dan gelen alanlar olacak hem de city alanı
+        public string City { get; set; }
+    }
+}
