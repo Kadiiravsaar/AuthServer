f3e1cb33a51042163b526fb9891e57f515575d8a
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-03 14:43:54 +0300

    Ders 9 IRepository interface'sinin oluşturulması

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();   
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity,bool>> predicate);
+
+        // where(x=>x.id>50)
+        // x => TEntity
+        // x.ıd>50 = bool
+
+        // product = prodrepo.where(x=>x.ıd>5) bu daha dbye yansımadı hala where sorgusu yazabilirsin
+        // product.tolist dersen dbye yansır
+
+        Task Add(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity); // _context.Entry(entity).state = entityState.modified
+
+    }
+}
