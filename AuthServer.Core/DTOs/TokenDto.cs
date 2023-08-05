3d68646e2e3ca72070fa1baf2ee6c0bff6525baf
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-05 14:56:18 +0300

    Ders13 TokenDto/ClientTokenDto

---
+++ AuthServer.Core/DTOs/TokenDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class TokenDto
+    {
+
+        public string AccessToken { get; set; } 
+        public DateTime AccessTokenExpiration { get; set; } // accesstoken'ın ömrünü belirliyorum
+        public string RefreshToken { get; set; } // accesstoken'ın ömrünü belirliyorum
+        public string RefreshTokenExpiration { get; set; } // RefresToken'ın ömrünü belirliyorum
+
+    }
+}
