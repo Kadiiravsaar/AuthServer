8d1e707a275406b5892c568269f562af9a555996
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-16 15:51:00 +0300

    ServiceGeneric added

--- AuthServer.Core/Services/IServiceGeneric.cs
+++ AuthServer.Core/Services/IServiceGeneric.cs
@@ -13,13 +13,13 @@ namespace AuthServer.Core.Services
         // (TDto) => Service katmanına data katmanından gelen entity dtoya çevirip direk api'ye sunacağım
         Task<Response<TDto>> GetByIdAsync(int id);
 
-        Task<Response<IEnumerable<TDto>>> GetAllAsync(); 
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
 
-        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TDto, bool>> predicate); // direk ham datayı çekeceğiz
+        Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate); // direk ham datayı çekeceğiz
 
-        Task<Response<TDto>> Add(TDto entity);
-        Task<Response<NoDataDto>> Remove(TEntity entity); // cilentlara boş data dönücem başka bir şey dönmeme gerek yok
-        Task<Response<NoDataDto>> Update(TEntity entity); // _context.Entry(entity).state = entityState.modified
+        Task<Response<TDto>> Add(TDto dto);
+        Task<Response<NoDataDto>> Remove(int id); // cilentlara boş data dönücem başka bir şey dönmeme gerek yok
+        Task<Response<NoDataDto>> Update(TDto dto, int id); // _context.Entry(entity).state = entityState.modified
 
     }
 }
