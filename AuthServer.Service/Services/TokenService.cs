c197b99f93f544f217035c5d654c5868a88a234e
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-17 16:24:17 +0300

    TokenService-1

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOptions _tokenOptions;
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOptions> options)
+        {
+            _userManager = userManager;
+            _tokenOptions = options.Value;
+        }
+
+        private string CreateRefreshToken() // bir tana refresh token üretecek 
+        {
+            var numberByte = new byte[32];
+            using var rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+            return Convert.ToBase64String(numberByte);
+        }
+
+
+        public TokenDto CreateToken(UserApp userApp) // kullanıcı ile işlem yapacağım o yüzden usermanager lazım
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
