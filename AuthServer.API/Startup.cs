6c121df9a7bb262783269a069428a87f19710898
Kadiiravsaar <kadiiravsar@gmail.com>
2023-08-28 14:08:55 +0300

    AuthenticaionService 1 aded

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -31,7 +31,7 @@ namespace AuthServer.API
             // CustomTokenOptions ve appsettingin haberleþmesini saðlayacaðýz
             services.Configure<CustomTokenOptions>(Configuration.GetSection("TokenOption"));
             // generic olarak CustomTokenOptions al. bu CustomTokenOptions'ý nerden al git appseetings içinden getsetting ile tokenoption al
-            services.Configure<Client>(Configuration.GetSection("Clients"));
+            services.Configure<List<Client>>(Configuration.GetSection("Clients"));
 
 
 
---
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UniwOfWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _clients; // cilentleri alacağım
+        private readonly ITokenService _tokenService; // token oluşturmak için
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _userRefreshToken;
+
+        public AuthenticationService(IOptions<List<Client>> optionsClients, ITokenService tokenService, UserManager<UserApp> userManager, 
+            IUnitOfWork unitOfWork, IGenericRepository<UserRefreshToken> userRefreshToken)
+        {
+            _clients = optionsClients.Value; // startup da okuyacak
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userRefreshToken = userRefreshToken;
+        }
+
+        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
